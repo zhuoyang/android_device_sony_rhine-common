b76070fa7d819718feff281eb38c17deee9b6902
zhuoyang <zhuoyang98@gmail.com>
2014-06-24 22:17:46 +0800

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,8 @@ BOARD_VENDOR_PLATFORM := rhine
 TARGET_CPU_VARIANT := krait
 
 # Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
 # Krait optimizations
